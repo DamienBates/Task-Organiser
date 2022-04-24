3b3c16eef1cdba67f306ecaffe10924b606d9763
DamienBates <80431557+DamienBates@users.noreply.github.com>
2022-04-24 18:17:22 +1000

    Separated index and App

--- ta-mern/src/index.tsx
+++ ta-mern/src/index.tsx
@@ -1,29 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Route, Routes } from 'react-router';
-import NavBar from './components/NavBar';
-import Home from './components/Home'
-import CreateTask from './components/CreateTask'
-import TaskList from './components/TaskList';
-import { Box } from '@mui/material';
-import { ThemeProvider } from '@mui/material';
-import CustomTheme from './CustomTheme';
-
-const App = () => {
-  return (
-    <Box>
-      <ThemeProvider theme={CustomTheme}>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/CreateTask' element={<CreateTask />} />
-          <Route path='/TaskList' element={<TaskList />} />
-        </Routes>
-      </ThemeProvider>
-    </Box>
-  );
-};
 
 ReactDOM.render(
   <React.StrictMode>
