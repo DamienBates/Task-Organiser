1a56c5dd0aaaf088f74640ec06b0e506d17cf005
DamienBates <80431557+DamienBates@users.noreply.github.com>
2022-04-24 18:16:43 +1000

    Update Home
    - Changed paddingLeft and paddingRight to padding

--- ta-mern/src/components/Home.tsx
+++ ta-mern/src/components/Home.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from '@material-ui/core'
 export default function Home() {
     return (
         <Box>
-            <Typography paragraph style={{ paddingLeft: '1rem', paddingTop: '0.8rem' }}>
+            <Typography paragraph style={{ padding: '1rem' }}>
                 Create your task using the 'Create Task' button at the top right. Indicate urgency
                 and then select 'Task List' to see your tasks.
             </Typography>
