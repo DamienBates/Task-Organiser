d032ea3e3873b94fdc3e2eae14ff13fa299101a4
DamienBates <80431557+DamienBates@users.noreply.github.com>
2022-04-26 21:20:30 +1000

    Update Home
    - Changed home screen welcome message
    - Added flexbox

--- ta-mern/src/components/Home.tsx
+++ ta-mern/src/components/Home.tsx
@@ -1,11 +1,19 @@
-import { Box, Typography } from '@material-ui/core'
+import { Typography } from "@mui/material"
+import { Box } from "@mui/system"
 
 export default function Home() {
     return (
-        <Box>
-            <Typography paragraph style={{ padding: '1rem' }}>
-                Create your task using the 'Create Task' button at the top right. Indicate urgency
-                and then select 'Task List' to see your tasks.
+        <Box style={{
+            display: "flex",
+            alignItems: "center",
+        }}>
+            <Typography
+                paragraph
+                style={{
+                    padding: "1rem"
+                }}
+            >
+                Welcome to my task organiser, hope you enjoy your stay.
             </Typography>
         </Box>
     )
