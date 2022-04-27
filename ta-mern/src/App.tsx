90053575ce7bcf25c611172e18f217371ac76db1
DamienBates <80431557+DamienBates@users.noreply.github.com>
2022-04-27 20:04:43 +1000

    Update App
    - Added global state handlers for the entirety of the CRUD lifecycle
    - Added fetching at the highest component
    - Minor naming update to routes

--- ta-mern/src/App.tsx
+++ ta-mern/src/App.tsx
@@ -1,36 +1,75 @@
-import { Route, Routes } from 'react-router';
-import { Box, CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material';
-import { TaskContext } from './TaskContext';
-import { useMemo, useState } from 'react';
-import CreateTask from './components/CreateTask';
-import Home from './components/Home'
-import TaskList from './components/TaskList';
-import CustomTheme from './CustomTheme';
-import DrawerComponent from './components/NavDrawer';
+import { Route, Routes } from "react-router";
+import { Box, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+import { TaskContext } from "./TaskContext";
+import { useEffect, useMemo, useState } from "react";
+import FormHandler from "./components/FormHandler";
+import Home from "./components/Home";
+import CustomTheme from "./CustomTheme";
+import DrawerComponent from "./components/NavDrawer";
+import axios from "axios";
+
+interface ToDoListProps {
+    task: string,
+    comments: string,
+    priority: string,
+}
+
+const todoList = {
+    task: "",
+    comments: "",
+    priority: ""
+}
 
 function App() {
-    const [globalTasks, setGlobalTasks] = useState<Array<any>>([]);
-    const [submitted, setSubmitted] = useState<boolean>(false);
+    const [apiReturn, setApiReturn] = useState<Array<any>>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [todo, setTodo] = useState<ToDoListProps>(todoList)
+    const [edited, setEdited] = useState<ToDoListProps>(todoList)
 
+    // Memoised values
     const memoisedContext = useMemo(() => ({
-        submitted,
-        setSubmitted,
-        globalTasks,
-        setGlobalTasks
-    }), [submitted, globalTasks])
+        todo, setTodo, apiReturn, setApiReturn, edited, setEdited, // Create, Read, & Update Handlers 
+        loading, setLoading, fetchTasks
+    }), [edited, todo, apiReturn, loading])
+
+    // Retrieve Mongo Tasks
+    async function fetchTasks() {
+        setLoading(true);
+
+        try {
+            await axios
+                .get(`${process.env.REACT_APP_PUBLIC_URL}`)
+                .then((response) => {
+                    setApiReturn(response.data)
+                    setLoading(false);
+                })
+        } catch (error) {
+            console.log("Something went wrong")
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    useEffect(() => {
+        if (loading === true) {
+            fetchTasks();
+        }
+    }, [loading]);
 
     return (
         <Box>
             <ThemeProvider theme={CustomTheme}>
                 <CssBaseline />
                 <DrawerComponent />
-                <Box paddingLeft="65px" style={{
-                }}>
+                <Box paddingLeft="80px">
                     <TaskContext.Provider value={memoisedContext}>
                         <Routes>
-                            <Route path='/' element={<Home />} />
-                            <Route path='/CreateTask' element={<CreateTask />} />
+                            <Route path="/" element={<Home />} />
+                            <Route path="/TaskList" element={<FormHandler />} />
                         </Routes>
                     </TaskContext.Provider>
                 </Box>
