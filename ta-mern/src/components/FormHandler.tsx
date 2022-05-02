193019bef143c2a03351670b48fb62aa7fffb363
DamienBates <80431557+DamienBates@users.noreply.github.com>
2022-05-02 13:10:40 +1000

    Update FormHandler
    - Minor formatting change
    - Added paddingLeft instead of marginLeft
    - Removed setTodo on submission as it caused a bug where consecutive additions to list caused an empty field

--- ta-mern/src/components/FormHandler.tsx
+++ ta-mern/src/components/FormHandler.tsx
@@ -1,10 +1,22 @@
-import { ChangeEvent, useContext, useState } from "react";
-import { TextField, Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Grid, Typography } from "@mui/material";
+import { ChangeEvent, useContext, useRef, useState } from "react";
+import {
+    TextField,
+    Box,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    SelectChangeEvent,
+    Grid,
+    Typography,
+    Slide,
+    FormControlLabel,
+    Switch
+} from "@mui/material";
 import { Button } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import { TaskContext } from "../TaskContext";
-import SendIcon from "@mui/icons-material/Send";
-import TaskList from "./UpdateDelete";
+import UpdateDelete from "./UpdateDelete";
 import axios from "axios"
 
 export default function FormHandler() {
@@ -14,8 +26,6 @@ export default function FormHandler() {
     // Global
     const { todo, setTodo, fetchTasks } = useContext(TaskContext);
 
-
-    // OnSubmit retrieval of tasks on Mongo database
     async function onSubmit(e: React.ChangeEvent<HTMLFormElement>) {
         e.preventDefault();
         setLoading(true);
@@ -29,12 +39,6 @@ export default function FormHandler() {
                 .then(() => {
                     fetchTasks();
                     e.target.reset();
-                    setTodo({
-                        ...todo,
-                        task: "",
-                        comments: "",
-                        priority: "_"
-                    })
                 })
         } catch (error) {
             console.log("Something went wrong retrieving tasks")
@@ -42,81 +46,94 @@ export default function FormHandler() {
     };
 
     return (
-        <form onSubmit={onSubmit}>
-            <Grid container
-                display="inline-flex"
-                padding="2vh 4vw 1.5vw 0"
-            >
-                <Grid item pb="10px" ml="25px">
-                    <TextField
-                        label="What needs to be done?"
-                        variant="standard"
-                        color="secondary"
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                            return setTodo({ ...todo, task: e.target.value })
-                        }}
-                    />
+        <section style={{
+            display: "flex",
+            justifyContent: "center",
+            width: "90vw"
+        }}>
+            <form onSubmit={onSubmit}>
+                <Grid
+                    container
+                    display="inline-flex"
+                    padding="4vh 4vw 1vw 0"
+                >
+                    <Grid item pb="10px" pl="25px">
+                        <TextField
+                            label="What needs to be done?"
+                            variant="standard"
+                            color="secondary"
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                return setTodo({ ...todo, task: e.target.value })
+                            }}
+                        />
+                    </Grid>
+                    <Grid item pb="10px" pl="25px">
+                        <TextField
+                            label="Any notes?"
+                            variant="standard"
+                            color="secondary"
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                return setTodo({ ...todo, comments: e.target.value })
+                            }}
+                        />
+                    </Grid>
+                    <Grid item pb="10px" pl="25px">
+                        <FormControl variant="standard" sx={{ minWidth: 195 }}>
+                            <InputLabel>
+                                How urgent?
+                            </InputLabel>
+                            <Select
+                                label="Priority"
+                                value={todo.priority}
+                                onChange={(e: SelectChangeEvent<string>) => {
+                                    return setTodo({ ...todo, priority: e.target.value })
+                                }}>
+                                <MenuItem value="Low">Not very</MenuItem>
+                                <MenuItem value="Medium">So so</MenuItem>
+                                <MenuItem value="High" >Extremely</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item>
+                        {loading ? // If loading === true, prevent submission and display loading spinner
+                            <LoadingButton
+                                loading
+                                disabled
+                                variant="contained"
+                                sx={{ mt: "15px", ml: "25px" }}
+                            >
+                                Add
+                            </LoadingButton>
+                            :
+                            <Button
+                                sx={{
+                                    mt: "15px",
+                                    ml: "25px"
+                                }}
+                                disabled={
+                                    todo.task !== "" || todo.comments !== "" || todo.priority != "" ?
+                                        false : true
+                                }
+                                variant="outlined"
+                                type="submit"
+                            >
+                                Add
+                            </Button>
+                        }
+                    </Grid>
                 </Grid>
-                <Grid item pb="10px" ml="25px">
-                    <TextField
-                        label="Any notes?"
-                        variant="standard"
-                        color="secondary"
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                            return setTodo({ ...todo, comments: e.target.value })
-                        }}
-                    />
-                </Grid>
-                <Grid item pb="10px" ml="25px">
-                    <FormControl variant="standard" sx={{ minWidth: 150 }}>
-                        <InputLabel>
-                            How urgent?
-                        </InputLabel>
-                        <Select
-                            label="Priority"
-                            value={todo.priority}
-                            onChange={(e: SelectChangeEvent<string>) => {
-                                return setTodo({ ...todo, priority: e.target.value })
-                            }}>
-                            <MenuItem value="Low">Not very</MenuItem>
-                            <MenuItem value="Medium">So so</MenuItem>
-                            <MenuItem value="High" >Extremely</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item>
-                    {loading ? // If loading === true, prevent submission and display loading spinner
-                        <LoadingButton
-                            loading
-                            disabled
-                            loadingPosition="end"
-                            endIcon={<SendIcon />}
-                            variant="contained"
-                            sx={{ mt: "15px", ml: "25px" }}
-                        >
-                            Add
-                        </LoadingButton>
-                        :
-                        <Button
-                            sx={{ mt: "15px", ml: "25px" }}
-                            disabled={todo.task !== "" && todo.comments !== "" && todo.priority != "" ?
-                                false : true
-                            }
-                            variant="outlined"
-                            type="submit"
-                        >
-                            Add
-                        </Button>
-                    }
-                </Grid>
-            </Grid>
-            <Box>
-                <TaskList />
-                <Typography fontSize={"10px"} pl="20px" pb="5px"
-                    style={{ opacity: "0.8" }}>
-                    Note: Cells can be edited.
-                </Typography>
-            </Box>
-        </form >
+                <Box>
+
+                    <UpdateDelete />
+
+                    <Typography
+                        fontSize={"10px"}
+                        padding={"5px 25px 10px 20px"}
+                        style={{ opacity: "0.8" }}>
+                        Note: Cells can be edited.
+                    </Typography>
+                </Box>
+            </form >
+        </section>
     )
 };
