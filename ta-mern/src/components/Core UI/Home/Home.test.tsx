f58ecfacb84a35449f3b6378c3dd7fea99f945a9
DamienBates <80431557+DamienBates@users.noreply.github.com>
2022-05-11 17:28:18 +1000

    Minor cleanup of Home test
    - Naming of description changed

--- ta-mern/src/components/Core UI/Home/Home.test.tsx
+++ ta-mern/src/components/Core UI/Home/Home.test.tsx
@@ -1,13 +1,11 @@
 import Home from "./Home"
-import React from "react"
 import renderer from 'react-test-renderer';
-import { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-describe("home page tests", function () {
+describe("home page", function () {
     test("text displays", () => {
         expect(Home).toBeDefined();
     });
