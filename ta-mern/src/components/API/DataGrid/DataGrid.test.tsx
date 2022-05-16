a4691008eec842373a9bd0fc079a0cf1e6224cb4
DamienBates <80431557+DamienBates@users.noreply.github.com>
2022-05-16 16:14:34 +1000

    Update DataGrid Tests
    - Added rendering, snapshot and typography testing
    - All passing

--- ta-mern/src/components/API/DataGrid/DataGrid.test.tsx
+++ ta-mern/src/components/API/DataGrid/DataGrid.test.tsx
@@ -1,8 +1,40 @@
-import DataGrid from "./DataGrid"
-import { render } from '@testing-library/react';
+import { screen, render, act, findByTestId } from '@testing-library/react'
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import DataGrid from './DataGrid';
+
+configure({ adapter: new Adapter() });
 
 describe("DataGrid", function () {
-    test("displays table", () => {
-        render(<DataGrid />)
+    beforeEach(() => {
+        jest.spyOn(React, 'useContext').mockImplementation(() => ({
+            todo: {
+                id: "mock",
+                tasks: "mock",
+                comments: "mock",
+                priority: "mock"
+            }
+        }));
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks;
+    })
+
+    it("renders", async () => {
+        shallow(<DataGrid />)
+    });
+
+    it("succeeds snapshot testing", async () => {
+        const myGrid = shallow(
+            <DataGrid />).dive();
+
+        expect(myGrid).toMatchSnapshot();
+    });
+
+    it("contains typography when data is empty", () => {
+        const myGrid = shallow(<DataGrid />);
+        expect(myGrid.contains('No tasks found, feel free to add some!')).toBe(false);
     });
 })
