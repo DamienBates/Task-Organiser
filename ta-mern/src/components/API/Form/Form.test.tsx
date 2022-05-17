a7bf3ea31055bf721436fb4b786cde60d31b4a75
DamienBates <80431557+DamienBates@users.noreply.github.com>
2022-05-17 10:23:37 +1000

    Update Form.test.tsx
    - Added useContext mock
    - Added render, snapshot and placeholder tests

--- ta-mern/src/components/API/Form/Form.test.tsx
+++ ta-mern/src/components/API/Form/Form.test.tsx
@@ -1,8 +1,39 @@
+import { shallow, configure } from "enzyme";
+import { fireEvent, queryByDisplayValue, render } from "@testing-library/react";
+import Adapter from "enzyme-adapter-react-16";
+import React from "react";
 import Form from "./Form";
-import { render } from '@testing-library/react';
 
-describe("form", function () {
-    test("displays text and form", () => {
-        render(<Form />)
-    });
+configure({ adapter: new Adapter() });
+
+beforeEach(() => {
+    jest.spyOn(React, 'useContext').mockImplementation(() => ({
+        todo: {
+            id: "form-mock",
+            tasks: "form-mock",
+            comments: "form-mock",
+            priority: "form-mock"
+        }
+    }));
+});
+
+afterEach(() => {
+    jest.clearAllMocks;
 })
+
+describe("Form", function () {
+    it("renders correctly", async () => {
+        shallow(<Form />);
+    });
+
+    it("initial snapshot matches", () => {
+        const component = shallow(<Form />);
+
+        expect(component.getElements()).toMatchSnapshot();
+    });
+
+    it("contains placeholder", () => {
+        const myGrid = shallow(<Form />);
+        expect(myGrid.contains('How urgent?')).toBe(true);
+    })
+});
